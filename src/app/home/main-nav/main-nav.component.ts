2f7433300776f51bf0090f5db66e84b8eef95e78
GabrielHSS <gabrielbet4@gmail.com>
2020-06-03 01:00:03 -0300

    Ajustes sidebar e rotas

--- src/app/home/main-nav/main-nav.component.ts
+++ src/app/home/main-nav/main-nav.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss']
 })
+
 export class MainNavComponent {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -15,7 +16,5 @@ export class MainNavComponent {
       map(result => result.matches),
       shareReplay()
     );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {}
 }
