c94d8cf747633d6ff36d7abcdd6e641f4b108dcf
Niggarilomon <losgamers123456@gmail.com>
2020-06-03 16:49:55 -0300

    Login e register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,20 +1,74 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export interface Login {
+  name: string,
+  password: string
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-    loginForm = this.fb.group({
-    name: [null, Validators.required],
+  loginForm = this.fb.group({
+    name: [null, [Validators.required, Validators.maxLength(60)]],
     password: [null, Validators.required],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private http: HttpClient,
+    private fb: FormBuilder
+  ) {}
 
   onSubmit() {
-    alert('Thanks!');
+    try {
+      if(!this.loginForm.valid) {
+        alert('Resolva todos os campos com erros')
+      } else {
+        this.doLogin({
+          name: this.loginForm.value.name.trim(),
+          password: this.loginForm.value.password.trim(),
+        }).subscribe((result) => {
+          window.localStorage.setItem('user', result['data']);
+          alert('Login')
+        });
+      }
+    } catch (error) {
+      console.log('Algo de errado aconteceu, por favor tente mais tarde');
+    }
   }
+
+  doLogin (login: Login): Observable<Login> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      })
+    };
+    return this.http.post<Login>('url', login, httpOptions)
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Aconteceu um error:', error.error.message);
+    } else {
+
+      console.error(
+        `Status Code: ${error.status}, ` +
+        `Body: ${error.error}`);
+    }
+    return throwError(
+      'Algo de errado aconteceu, por favor tente mais tarde'
+    );
+  };
+
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export interface Register {
+  name: string,
+  last_name: string,
+  nickname: string,
+  email: string,
+  password: string,
+}
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,14 +20,18 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 
 export class RegisterComponent {
-    addressForm = this.fb.group({
-    name: [null, Validators.required],
-    last_name: [null, Validators.required], 
-    nickname: [null, Validators.required],
-    email: [null, Validators.required],
+  addressForm = this.fb.group({
+    name: [null, [Validators.required, Validators.maxLength(60)]],
+    last_name: [null, [Validators.required, Validators.maxLength(60)]],
+    nickname: [null, [Validators.required, Validators.maxLength(60)]],
+    email: [null, [Validators.required, Validators.maxLength(60), Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
     password: [null, Validators.required],
   });
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {}
+
   mobile = false;
   getmobile(): boolean {
     const w = document.documentElement.clientWidth;
@@ -34,8 +50,53 @@ export class RegisterComponent {
       this.mobile = this.getmobile();
     };
   }
-  
+
   onSubmit() {
-    alert('Thanks!');
+    try {
+      if(!this.addressForm.valid) {
+        alert('Resolva todos os campos com erros')
+      } else {
+        this.doRegister({
+          name: this.addressForm.value.name.trim(),
+          last_name: this.addressForm.value.last_name.trim(),
+          nickname: this.addressForm.value.nickname.trim(),
+          email: this.addressForm.value.email.trim(),
+          password: this.addressForm.value.password.trim(),
+        }).subscribe((result) => {
+          window.localStorage.setItem('user', result['data']);
+          alert('Register')
+        });
+      }
+    } catch (error) {
+      console.log('Algo de errado aconteceu, por favor tente mais tarde');
+    }
   }
+
+  doRegister (register: Register): Observable<Register> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      })
+    };
+    return this.http.post<Register>('url', register, httpOptions)
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Aconteceu um error:', error.error.message);
+    } else {
+
+      console.error(
+        `Status Code: ${error.status}, ` +
+        `Body: ${error.error}`);
+    }
+    return throwError(
+      'Algo de errado aconteceu, por favor tente mais tarde'
+    );
+  };
+
 }
